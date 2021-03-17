0283b0a8d344943d81956ae08f7a8613231312a7
Gina Vargas <gina.vargas89@outlook.com>
2021-03-16 22:50:44 -0700

    adding require and import

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require ("express");
+const path = require ("path");
+const fs = require ("fs");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.envPort || 3080;
