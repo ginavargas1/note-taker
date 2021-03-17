81e249c40aadc2b503a3d6d1eef77be064b765b8
Gina Vargas <gina.vargas89@outlook.com>
2021-03-17 15:52:48 -0700

    adding express and app

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+//add dependencies
 const express = require ("express");
 const path = require ("path");
 const fs = require ("fs");
 const bodyParser = require("body-parser");
 
+// add express app and process.env
 const app = express();
-const PORT = process.envPort || 3080;
+const PORT = process.env.PORT || 3080;
 
-app.use
+//set express app 
+app.use(express.static('public'));
+
+//routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+
+
+
+
 
